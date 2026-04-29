b66d1ab98b3aec5f64733b4dfd73fd7dc5f865c7
Fabian <fabian@gmail.com>
2026-04-29 10:41:20 -0600

    Estilos movidos a archivo externo y limpieza de PHP

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 // conexion.php
-$conexion = new mysqli("localhost", "fescobar_db", "fabian$2005$", "fescobar_db");
+$conexion = new mysqli("localhost", "fescobar_db", 'fabian$2005$', "fescobar_db");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
