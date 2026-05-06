ca18c5f2add19a1c6929c5b9c5aed6415b9faf33
Fabian <fabian@gmail.com>
2026-05-05 22:34:11 -0600

    Intento de conexión con credenciales actualizadas y estilos CSS

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 // 1. Prueba con 127.0.0.1 en lugar de localhost para evitar bloqueos de socket
 $host = "127.0.0.1"; 
 $user = "fescobar_db";
-$pass = "12345678"; 
+$pass = ""; 
 $db   = "fescobar_db";
 
 // Reporte de errores para saber exactamente qué falla
