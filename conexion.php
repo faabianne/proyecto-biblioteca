525998a73c13bdedd1cd6b1fc3f17c636962568f
Fabian <fabian@gmail.com>
2026-05-05 22:22:20 -0600

    Estructura wwwroot, estilos CSS y corrección de conexión DB

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 // conexion.php
 // Prueba con 127.0.0.1 si localhost falla
-$conexion = new mysqli("127.0.0.1", "fescobar_db", 'fabian$2005$', "fescobar_db");
+$conexion = new mysqli("127.0.0.1", "fescobar_db", "12345678", "fescobar_db");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
